a18b6cfb9094cbcc745b8344a2948ce8668268bc
Raulmoyano2970 <raulmoyano2970@gmail.com>
2024-06-30 08:25:42 -0300

    a

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import commentRoutes from './routes/comment.route.js';
 import comment2Routes from './routes/comment2.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -22,6 +23,13 @@ mongoose
 
 const __dirname = path.resolve();
 const app = express();
+
+// Configurar CORS
+const corsOptions = {
+  origin: 'http://localhost:3000', // Reemplaza con la URL de tu front-end
+  credentials: true, // Si necesitas enviar cookies o encabezados de autorización
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -38,8 +46,6 @@ app.use('/api/comment2', comment2Routes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-
-
 // Ruta para servir el archivo index.html desde la carpeta client/dist para todas las demás rutas
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
@@ -55,3 +61,62 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
+
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// import userRoutes from './routes/user.route.js';
+// import authRoutes from './routes/auth.route.js';
+// import postRoutes from './routes/post.route.js';
+// import commentRoutes from './routes/comment.route.js';
+// import comment2Routes from './routes/comment2.route.js';
+// import cookieParser from 'cookie-parser';
+// import path from 'path';
+
+// dotenv.config();
+
+// mongoose
+//   .connect(process.env.MONGO)
+//   .then(() => {
+//     console.log('MongoDb is connected');
+//   })
+//   .catch((err) => {
+//     console.error('MongoDb connection error:', err);
+//   });
+
+// const __dirname = path.resolve();
+// const app = express();
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.listen(3000, () => {
+//   console.log('Server is running on port 3000!');
+// });
+
+// app.use('/api/user', userRoutes);
+// app.use('/api/auth', authRoutes);
+// app.use('/api/post', postRoutes);
+// app.use('/api/comment', commentRoutes);
+// app.use('/api/comment2', comment2Routes);
+
+// app.use(express.static(path.join(__dirname, '/client/dist')));
+
+
+
+// // Ruta para servir el archivo index.html desde la carpeta client/dist para todas las demás rutas
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
+
+// // Middleware para manejar errores
+// app.use((err, req, res, next) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || 'Internal Server Error';
+//   res.status(statusCode).json({
+//     success: false,
+//     statusCode,
+//     message,
+//   });
+// });
