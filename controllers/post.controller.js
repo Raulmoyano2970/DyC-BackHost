5da0634d9e44a2526905a28e167e4bebf46629ab
Raulmoyano2970 <raulmoyano2970@gmail.com>
2024-06-30 22:41:39 -0300

    arreglo

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,26 +1,31 @@
 import Post from '../models/post.model.js';
 import { errorHandler } from '../utils/error.js';
 
+// Función para crear un nuevo post
 export const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
     return next(errorHandler(403, 'You are not allowed to create a post'));
   }
+
   const requiredFields = ['title', 'content', 'contenido', 'celular', 'celularemergencia', 'email', 'edad', 'direccion', 'sanguineo'];
   for (const field of requiredFields) {
     if (!req.body[field]) {
       return next(errorHandler(400, `Por favor completar todos los campos: completar ${field} `));
     }
   }
+
   const slug = req.body.title
     .split(' ')
     .join('-')
     .toLowerCase()
     .replace(/[^a-zA-Z0-9-]/g, '');
+
   const newPost = new Post({
     ...req.body,
     slug,
     userId: req.user.id,
   });
+
   try {
     const savedPost = await newPost.save();
     res.status(201).json(savedPost);
@@ -29,12 +34,13 @@ export const create = async (req, res, next) => {
   }
 };
 
-
+// Función para obtener los posts
 export const getposts = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
     const sortDirection = req.query.order === 'asc' ? 1 : -1;
+
     const posts = await Post.find({
       ...(req.query.userId && { userId: req.query.userId }),
       ...(req.query.category && { category: req.query.category }),
@@ -51,6 +57,7 @@ export const getposts = async (req, res, next) => {
       .sort({ updatedAt: sortDirection })
       .skip(startIndex)
       .limit(limit);
+
     const totalPosts = await Post.countDocuments();
     const now = new Date();
     const oneMonthAgo = new Date(
@@ -58,9 +65,11 @@ export const getposts = async (req, res, next) => {
       now.getMonth() - 1,
       now.getDate()
     );
+
     const lastMonthPosts = await Post.countDocuments({
       createdAt: { $gte: oneMonthAgo },
     });
+
     res.status(200).json({
       posts,
       totalPosts,
@@ -71,10 +80,12 @@ export const getposts = async (req, res, next) => {
   }
 };
 
+// Función para eliminar un post
 export const deletepost = async (req, res, next) => {
-  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin) {
     return next(errorHandler(403, 'You are not allowed to delete this post'));
   }
+
   try {
     await Post.findByIdAndDelete(req.params.postId);
     res.status(200).json('The post has been deleted');
@@ -83,10 +94,12 @@ export const deletepost = async (req, res, next) => {
   }
 };
 
+// Función para actualizar un post
 export const updatepost = async (req, res, next) => {
-  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin) {
     return next(errorHandler(403, 'You are not allowed to update this post'));
   }
+
   try {
     const updatedPost = await Post.findByIdAndUpdate(
       req.params.postId,
@@ -100,12 +113,13 @@ export const updatepost = async (req, res, next) => {
           email: req.body.email,
           edad: req.body.edad,
           category: req.body.category,
-          sanguineo:req.body.sanguineo,
-          direccion:req.body.direccion,
+          sanguineo: req.body.sanguineo,
+          direccion: req.body.direccion,
         },
       },
       { new: true }
     );
+
     res.status(200).json(updatedPost);
   } catch (error) {
     next(error);
